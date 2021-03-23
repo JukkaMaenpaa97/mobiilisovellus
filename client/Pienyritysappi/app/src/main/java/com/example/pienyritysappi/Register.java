fd989ef689c5513a810268b44a0c9f16cca7c412
Krymyy <p-eetu@hotmail.com>
2021-03-23 12:41:36 +0200

    register valmis

--- client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/Register.java
+++ client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/Register.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
 
@@ -11,7 +12,12 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Intent intent = getIntent();
 
+    }
+
+    public void button8Clicked(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
     }
 }
