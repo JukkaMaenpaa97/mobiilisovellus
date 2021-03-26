3e80353ab520729fe6a773052fcc9bc291ee2996
TheR0z0r <aus435@hotmail.com>
2021-03-26 13:25:23 +0200

    Updating database handler
    Adding some error handling

--- service/database/database.py
+++ service/database/database.py
@@ -4,10 +4,23 @@ mysql = MySQL()
 
 def query(query, params, single = False):
     cursor = mysql.connection.cursor()
-    cursor.execute(query, params)
-    mysql.connection.commit()
-    if single:
-        result = cursor.fetchone()
-    else:
-        result = cursor.fetchall()
-    return result
+
+    try:
+        cursor.execute(query, params)
+        mysql.connection.commit()
+        if not cursor.rowcount:
+            return None
+            
+    except (MySQLdb.Error, MySQLdb.Warning) as e:
+        print(e)
+        return None
+
+    try:
+        if single:
+            result = cursor.fetchone()
+        else:
+            result = cursor.fetchall()
+        return result
+    except TypeError as e:
+        print(e)
+        return None
