b696dd0cf5d161416a19c88d5f82493a3ad7833b
Ville Prittinen <villepri@hotmail.com>
2021-03-30 13:00:49 +0300

    JsonParse Category javafiluun (ei toimi)

--- client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/Category.java
+++ client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/Category.java
@@ -5,21 +5,64 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Category extends AppCompatActivity {
+
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
         Intent intent = getIntent();
+        mTextViewResult = findViewById(R.id.textViewCategoryName);
+        mQueue = Volley.newRequestQueue(this);
         jsonParse();
-
     }
 
     private void jsonParse(){
-        String url = "http://mobiilisovellus.therozor.com:5000/";
+        String url = "http://mobiilisovellus.therozor.com:5000/categories";
+        System.out.println("kai mun private void jsonParse toimii");
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("data");
 
+                            for (int i = 0; i < jsonArray.length(); i++){
+                                JSONObject category = jsonArray.getJSONObject(i);
+                                System.out.println(category);
+                                String categoryName = category.getString("category_name");
+                                System.out.println(categoryName);
+                                mTextViewResult.setText(categoryName);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
     }
 
     public void homeButtonClicked(View view) {
@@ -41,4 +84,5 @@ public class Category extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(),Company.class);
         startActivity(intent);
     }
+
 }
