41c6188626ecec0f95a9d22baa4d51b80d9f24f9
Krymyy <p-eetu@hotmail.com>
2021-03-30 13:27:08 +0300

    asiakasprofiilia muutettu

--- client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/CustomerProfile.java
+++ client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/CustomerProfile.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CustomerProfile extends AppCompatActivity {
 
@@ -12,6 +13,19 @@ public class CustomerProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_profile);
+
+        EditText mEdit = (EditText) findViewById(R.id.editTextTextPersonName3);
+        mEdit.setEnabled(false);
+
+        EditText mEdit2 = (EditText) findViewById(R.id.editTextTextEmailAddress2);
+        mEdit2.setEnabled(false);
+
+        EditText mEdit3 = (EditText) findViewById(R.id.editTextTextPassword2);
+        mEdit3.setEnabled(false);
+
+        EditText mEdit4 = (EditText) findViewById(R.id.editTextTextPassword3);
+        mEdit4.setEnabled(false);
+
     }
 
 
@@ -19,5 +33,34 @@ public class CustomerProfile extends AppCompatActivity {
     {
         Intent intent = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(intent);
+    }
+
+    public void tietojenmuokkaus(View view) {
+        EditText mEdit = (EditText) findViewById(R.id.editTextTextPersonName3);
+        mEdit.setEnabled(true);
+
+        EditText mEdit2 = (EditText) findViewById(R.id.editTextTextEmailAddress2);
+        mEdit2.setEnabled(true);
+
+        EditText mEdit3 = (EditText) findViewById(R.id.editTextTextPassword2);
+        mEdit3.setEnabled(true);
+
+        EditText mEdit4 = (EditText) findViewById(R.id.editTextTextPassword3);
+        mEdit4.setEnabled(true);
+    }
+
+    public void tallennatiedot(View view) {
+        EditText mEdit = (EditText) findViewById(R.id.editTextTextPersonName3);
+        mEdit.setEnabled(false);
+
+        EditText mEdit2 = (EditText) findViewById(R.id.editTextTextEmailAddress2);
+        mEdit2.setEnabled(false);
+
+        EditText mEdit3 = (EditText) findViewById(R.id.editTextTextPassword2);
+        mEdit3.setEnabled(false);
+
+        EditText mEdit4 = (EditText) findViewById(R.id.editTextTextPassword3);
+        mEdit4.setEnabled(false);
+
     }
 }
