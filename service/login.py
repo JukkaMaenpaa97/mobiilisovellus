4b3ca019e99528522da3f152f9aaaf0e701ec12d
a-arol <n4arat00@students.oamk.fi>
2021-03-30 12:37:11 +0300

    Virheenkäsittely ja nimimuutoksia.

--- service/login.py
+++ service/login.py
@@ -15,7 +15,7 @@ class Api(DataModel):
         self.table = "sessions" 
 
         self.fields = {
-            "session_id":          self.field(),
+            "session_id":     self.field(),
             "session_apikey":   self.field(),
             "session_user_id": self.field(),
             "session_created": self.field(),
@@ -26,29 +26,29 @@ class Api(DataModel):
 
 class Login(Resource):
       def post(self):
-        testset = Api()
-        time_created=""
-        time_expires=""
-        data = request.form
-        credentials = {"user_email": data.get('user_email'), "user_password": hashlib.sha256(data.get('user_password').encode('utf-8')).hexdigest() }
-        #tosend{"user_id":, "session_apikey":}
-        crendentialcheck = query("SELECT user_email, user_password FROM users WHERE user_email = %(user_email)s AND user_password = %(user_password)s", credentials)
-        getid = query("SELECT user_id FROM users WHERE user_email = %(user_email)s AND user_password = %(user_password)s", credentials,True)
-        print(getid)
-        #check if query returned rows
-        if(credentials is None):
-            return {"message": "invalid_credentials"}
-        else:
-            apikey = Auth.createApiKey()
-            #unix integer timestamps for now and expiring date
-            time_created = int(time.time())
-            time_expires = time_created + 172800
-            testset.set("session_user_id", getid["user_id"])
-            testset.set("session_apikey", apikey)
-            testset.set("session_created",time_created)
-            testset.set("session_expires",time_expires)
-            testset.create()
-            return {"message": "login_success"}
+            getapi = Api()
+            time_created=""
+            time_expires=""
+            data = request.form
+            credentials = {"user_email": data.get('user_email'), "user_password": hashlib.sha256(data.get('user_password').encode('utf-8')).hexdigest() }
+            crendentialcheck = query("SELECT user_email, user_password FROM users WHERE user_email = %(user_email)s AND user_password = %(user_password)s", credentials)
+            getid = query("SELECT user_id FROM users WHERE user_email = %(user_email)s AND user_password = %(user_password)s", credentials,True)
+            try:
+                  apikey = Auth.createApiKey()
+                  #unix integer timestamps for now and expiring date
+                  time_created = int(time.time())
+                  time_expires = time_created + 172800
+                  getapi.set("session_user_id", getid["user_id"])
+                  getapi.set("session_apikey", apikey)
+                  getapi.set("session_created",time_created)
+                  getapi.set("session_expires",time_expires)
+                  getapi.create()
+                  return {"message": "login_success","apikey": apikey}
+
+            except Exception:
+                  return {"message": "invalid credentials"}
+                     
+            
       
 
       
