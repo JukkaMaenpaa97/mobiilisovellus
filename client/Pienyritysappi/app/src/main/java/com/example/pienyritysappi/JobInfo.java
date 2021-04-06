ef123f4034cdae2b968c328fbad49a97e0d3a885
Ville Prittinen <villepri@hotmail.com>
2021-04-06 10:56:57 +0300

    JobInfo:ssa on nyt myös Json kysely

--- client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/JobInfo.java
+++ client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/JobInfo.java
@@ -5,13 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class JobInfo extends AppCompatActivity {
+
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_info);
+        mTextViewResult = findViewById(R.id.header);
+        mQueue = Volley.newRequestQueue(this);
+        jsonParse();
+
+    }
+
+    private void jsonParse(){
+        String url = "http://mobiilisovellus.therozor.com:5000/services";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("data");
+                            JSONObject job = jsonArray.getJSONObject(1);   //tähän indeksi monennenko työn haluaa (0=eka 1=toka jne. tullaan vaihtamaan id:llä haettavaksi)
+                            String jobName = job.getString("service_title");
+                            mTextViewResult.setText(jobName);
+
+                        } catch (JSONException e) {
+                            System.out.println("\nnyt ollaan onResponsen catchissä: JSONExceptionissa siis\n");
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("onErrorResponsessa ollaan.");
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
     }
 
     public void homeButtonClicked(View view)
