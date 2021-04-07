13867aff57f6b86fe92b76e05e514068657f4a41
TheR0z0r <aus435@hotmail.com>
2021-04-07 07:16:40 +0300

    User route now returns current users information

--- service/routes/user.py
+++ service/routes/user.py
@@ -5,8 +5,21 @@ from datamodels.usermodel import UserModel
 
 class User(Resource):
     def get(self, id):
+        if id == "me":
+            current_user = Auth.checkApiKey()
+            if not current_user:
+                return Auth.unauthorizedResponse()
+            return {
+                "message": "Omat tiedot",
+                "data": current_user.serialize(include_private=["user_email", "user_phone", "user_address", "user_postalcode", "user_city"])
+            }, 200
+
         usermodel = UserModel()
+
         if usermodel.load(id):
-            return {"data": usermodel.serialize()}, 200
+            return {
+                "message": "Toisen käyttäjän tiedot",
+                "data": usermodel.serialize()
+            }, 200
         else:
             return {"message": "Käyttäjää ei löytynyt"}, 404
