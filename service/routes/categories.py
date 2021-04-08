ddb26a3478eed5093970c8089a0e8f0b9137ede2
a-arol <n4arat00@students.oamk.fi>
2021-04-08 12:41:27 +0300

    Kategorian päivitys

--- service/routes/categories.py
+++ service/routes/categories.py
@@ -5,66 +5,47 @@ from flask import request
 from validate.postvalidator import PostValidator
 
 
+
 class Categories(Resource):
-#vielä työn alla
-        def put(self):
-            category = CategoryModel()
-            data = request.form
-            validator = PostValidator()
-            validator.postData(data)
-            validator.addField(
-                "category_name",
-                validate = ["not_empty"]
-                )
-            validator.addField(
-                "category_description",
-                validate = ["not_empty"])
-
-            validation_result = validator.validate()
-
-            #categoryid = data.get("category_id")
-            #print(categoryid)
-
-#tästä alaspäin toimii
-
+                                                         
         def post(self):
-            categorymodel = CategoryModel()
-            data = request.form
-            validator = PostValidator()
-            validator.postData(data)
-            validator.addField(
+                categorymodel = CategoryModel()
+                data = request.form
+                validator = PostValidator()
+                validator.postData(data)
+                validator.addField(
                 "category_name",
                 validate = ["not_empty"]
                 )
-            validator.addField(
+                validator.addField(
                 "category_description",
                 validate = ["not_empty"])
 
-            validation_result = validator.validate()
+                validation_result = validator.validate()
 
-            if validation_result:
-                new_category = CategoryModel()
-                new_category.set("category_name", validator.get("category_name"))
-                new_category.set("category_description", validator.get("category_description"))
-                new_category.create()
-                return{"message": "Kategorian luonti onnistui"}, 200
-            else:
-                return{"message": "Kategorian luonti epäonnistui"}, 400
+                if validation_result:
+                        new_category = CategoryModel()
+                        new_category.set("category_name", validator.get("category_name"))
+                        new_category.set("category_description", validator.get("category_description"))
+                        new_category.create()
+                        return{"message": "Kategorian luonti onnistui"}, 200
+                else:
+                        return{"message": "Kategorian luonti epäonnistui"}, 400
 
         def get(self):
-            categorymodel = CategoryModel()
-            categorylist = DataModelList(CategoryModel)
+                categorymodel = CategoryModel()
+                categorylist = DataModelList(CategoryModel)
 
-            if(categorylist.load("SELECT "+categorymodel.getFields()+" FROM "+categorymodel.getTable())):
-                categorylist.computeAll()
-                return {
-                    "message": "Kategorioiden listaus",
-                    "count": categorylist.count(),
-                    "data": categorylist.serialize()
-                }, 200
-            else:
-                return {"message": "Yhtään kategoriaa ei löytynyt"}, 404
+                if(categorylist.load("SELECT "+categorymodel.getFields()+" FROM "+categorymodel.getTable())):
+                        categorylist.computeAll()
+                        return {
+                        "message": "Kategorioiden listaus",
+                        "count": categorylist.count(),
+                        "data": categorylist.serialize()
+                        }, 200
+                else:
+                        return {"message": "Yhtään kategoriaa ei löytynyt"}, 404
 
 
 
-    
+            
--- service/routes/category.py
+++ service/routes/category.py
@@ -3,12 +3,15 @@ from flask import request
 from database.database import mysql,query
 from security.auth import Auth
 from datamodels.categorymodel import CategoryModel
+from validate.postvalidator import PostValidator
+
 
 
 
         
 
-class Category(Resource):  
+class Category(Resource):
+                        
     def get(self,id):
         categorymodel = CategoryModel()
         if categorymodel.load(id):
@@ -16,8 +19,32 @@ class Category(Resource):
         else:
             return {"message": "Kategoriaa ei löytynyt"}, 404
         
+
+
+
+    def put(self,id):
+        categoryupdate = CategoryModel()
+        if categoryupdate.load(id):
+                data = request.form
+                validator = PostValidator()
+                validator.postData(data)
+                validator.addField(
+                "category_name",
+                validate = ["not_empty"]
+                )
+                
+                validator.addField(
+                "category_description",
+                validate = ["not_empty"])
+                
+                validation_result = validator.validate()
+                if validation_result:
+                        categoryupdate.set("category_name", validator.get("category_name"))
+                        categoryupdate.set("category_description", validator.get("category_description"))
+                        categoryupdate.update()
+                        return{"message": "Kategorian päivitys onnistui"}, 200
+                else:
+                        return{"message": "Virheelliset tiedot, päivitys epäonnistui"}, 400
         
-        
-        
-        
-        
+        else:
+                return{"message": "Kategorian päivitys epäonnistui"}, 400
