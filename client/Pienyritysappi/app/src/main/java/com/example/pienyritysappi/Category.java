abf2c53f19c84f680635e69906841b8112b2b729
Ville Prittinen <villepri@hotmail.com>
2021-04-09 13:05:01 +0300

    dynaamisesti luodut napit toimii niinku pitäski

--- client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/Category.java
+++ client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/Category.java
@@ -24,10 +24,13 @@ public class Category extends AppCompatActivity {
 
     private TextView mTextViewCategoryName;
     private RequestQueue mQueue;
-    String categoryurl;
-    private int serviceCount=1;
-    private String companyUserId="";
+    private String categoryurl;
+    private int companyCount =1;
     private Button nButton;
+    private String companyName;
+    private String contactName;
+    private JSONArray jsonArray;
+    private JSONObject company;
 
 
     @Override
@@ -39,86 +42,21 @@ public class Category extends AppCompatActivity {
 
         mTextViewCategoryName = findViewById(R.id.textViewCategoryName);
         mQueue = Volley.newRequestQueue(this);
-        jsonParseCompanyName();
-        jsonParseCompanyCount();
-        for(int i = 0; i<=serviceCount; i++) {
-            addButton(i); //luo myös onClickListenerin
-            jsonParseButtons(i);
-        }
+        jsonParse();
     }
 
-    private void addButton(int i) {
-        GridLayout layout = (GridLayout)findViewById(R.id.CompanyButtonGridLayout);
-        nButton = new Button(this);
-        nButton.setText(Integer.toString(i));
-        nButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                buttonCompanyClicked();
-            }
-        });
-        layout.addView(nButton);
-    }
-
-    private void jsonParseCompanyName() {
+    private void jsonParse() {
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, categoryurl, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
+                            jsonArray = response.getJSONArray("data");
+                            companyCount = response.getInt("count");
                             mTextViewCategoryName.setText(response.getString("category_name"));
-
-
-                        } catch (JSONException e) {
-                            System.out.println("\nnyt ollaan onResponsen catchissä: JSONExceptionissa\n");
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                System.out.println("onErrorResponsessa ollaan.");
-                error.printStackTrace();
-            }
-        });
-        mQueue.add(request);
-    }
-
-    private void jsonParseCompanyCount() {
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, categoryurl, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            serviceCount = response.getInt("count");
-
-
-                        } catch (JSONException e) {
-                            System.out.println("\nnyt ollaan onResponsen catchissä: JSONExceptionissa\n");
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                System.out.println("onErrorResponsessa ollaan.");
-                error.printStackTrace();
-            }
-        });
-        mQueue.add(request);
-    }
-
-    private void jsonParseButtons(int i) {
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, categoryurl, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("data");
-                            JSONObject category = jsonArray.getJSONObject(i);   //tähän indeksi monennenko haluaa (0=eka 1=toka jne. tullaan vaihtamaan id:llä haettavaksi)
-                            String companyName = category.getString("user_company_name");
-                            String contactName = category.getString("user_name");
-
+                            for(int i = 0; i< companyCount; i++) {
+                                addButton(i); //luo myös onClickListenerin
+                            }
 
                         } catch (JSONException e) {
                             System.out.println("\nnyt ollaan onResponsen catchissä: JSONExceptionissa\n");
@@ -135,6 +73,30 @@ public class Category extends AppCompatActivity {
         mQueue.add(request);
     }
 
+    private void addButton(int i) {
+        GridLayout layout = (GridLayout)findViewById(R.id.CompanyButtonGridLayout);
+        nButton = new Button(this);
+        try {
+            company = jsonArray.getJSONObject(i);
+            companyName = company.getString("user_company_name");
+            contactName = company.getString("user_name");
+            String buttonText = companyName + "\nYhteyshenkilö: " + contactName;
+            String companyUserId = company.getString("user_id");
+            nButton.setText(buttonText);
+            nButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(),Company.class);
+                    intent.putExtra("userId", companyUserId);
+                    startActivity(intent);
+                }
+            });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        layout.addView(nButton);
+    }
+
     public void homeButtonClicked(View view) {
         Intent intent = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(intent);
@@ -144,39 +106,4 @@ public class Category extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(),CustomerProfile.class);
         startActivity(intent);
     }
-
-    public void buttonCompanyClicked() {
-        jsonParseUserId();
-
-    }
-
-    private void jsonParseUserId() {
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, categoryurl, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("data");
-                            JSONObject category = jsonArray.getJSONObject(0);   //tähän indeksi monennenko haluaa (0=eka 1=toka jne. tullaan vaihtamaan id:llä haettavaksi)
-                            companyUserId = category.getString("user_id");
-                            System.out.println("companyUserId: " + companyUserId);
-                            Intent intent = new Intent(getApplicationContext(),Company.class);
-                            intent.putExtra("userId", companyUserId);
-                            startActivity(intent);
-
-                        } catch (JSONException e) {
-                            System.out.println("\nnyt ollaan onResponsen catchissä: JSONExceptionissa\n");
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                System.out.println("onErrorResponsessa ollaan.Category.java jsonParseUserId");
-                error.printStackTrace();
-            }
-        });
-        mQueue.add(request);
-    }
-
 }
