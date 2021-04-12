c036b38068be522d5cb9e67b597c170c49e57db9
Ville Prittinen <villepri@hotmail.com>
2021-04-12 11:52:24 +0300

    ajanvarauksessa dynaamisesti about 60 nappia

--- client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/ReservationActivity.java
+++ client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/ReservationActivity.java
@@ -5,13 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 public class ReservationActivity extends AppCompatActivity {
+
+    private Button nButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
+
+        for(int i = 0; i < 60; i++){
+            addButton(i);
+        }
+    }
+
+    private void addButton(int i) {
+        GridLayout layout = (GridLayout)findViewById(R.id.reservationGridLayout);
+        nButton = new Button(this);
+        String buttonText = "aika " + Integer.toString(i+1);
+        nButton.setText(buttonText);
+        nButton.setTextSize(20);
+        nButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        layout.addView(nButton);
     }
 
     public void homeButtonClicked(View view) {
