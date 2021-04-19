0cbc1dec6c551944a6c35afc51ad723c15027a32
Ville Prittinen <villepri@hotmail.com>
2021-04-19 11:36:53 +0300

    AddJob nappi lisätty MainActivityyn

--- client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/AddJob.java
+++ client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/AddJob.java
@@ -3,6 +3,7 @@ package com.example.pienyritysappi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AddJob extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class AddJob extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_job);
+    }
+
+    public void addJobConfirmClicked(View view) {
+        //wtfamisupposedtodohere?
     }
 }
