66745aff5769e73ece3f391078bba0ef49a7a1a6
TheR0z0r <aus435@hotmail.com>
2021-04-20 10:30:41 +0300

    Koodin refaktorointia ja käyttäjän tyypin tarkistuksen lisäys
    Vain admineilla on oikeus muokata kategorioita

--- service/routes/categories.py
+++ service/routes/categories.py
@@ -1,51 +1,58 @@
 from flask_restful import Resource
+from flask import request
 from datamodels.datamodellist import DataModelList
 from datamodels.categorymodel import CategoryModel
-from flask import request
+from datamodels.usermodel import UserModel
+from security.auth import Auth
 from validate.postvalidator import PostValidator
 
-
-
 class Categories(Resource):
-                                                         
-        def post(self):
-                categorymodel = CategoryModel()
-                data = request.form
-                validator = PostValidator()
-                validator.postData(data)
-                validator.addField(
-                "category_name",
-                validate = ["not_empty"]
-                )
-                validator.addField(
-                "category_description",
-                validate = ["not_empty"])
+    def post(self):
+        # checking if user is logged in
+        current_user = Auth.checkApiKey()
+        if not current_user:
+            return Auth.unauthorizedResponse()
 
-                validation_result = validator.validate()
+        # if user is not an admin, don't allow modification
+        if current_user.get("user_type") != 3:
+            return Auth.unauthorizedResponse()
 
-                if validation_result:
-                        new_category = CategoryModel()
-                        new_category.set("category_name", validator.get("category_name"))
-                        new_category.set("category_description", validator.get("category_description"))
-                        new_category.create()
-                        return{"message": "Kategorian luonti onnistui"}, 200
-                else:
-                        return{"message": "Kategorian luonti epäonnistui"}, 400
+        validator = PostValidator()
+        validator.postData(request.json)
+        validator.addField("category_name", validate = ["not_empty"])
+        validator.addField("category_description", validate = ["not_empty"])
 
-        def get(self):
-                categorymodel = CategoryModel()
-                categorylist = DataModelList(CategoryModel)
+        if validator.validate():
+            new_category = CategoryModel()
+            new_category.set("category_name", validator.get("category_name"))
+            new_category.set("category_description", validator.get("category_description"))
 
-                if(categorylist.load("SELECT "+categorymodel.getFields()+" FROM "+categorymodel.getTable())):
-                        categorylist.computeAll()
-                        return {
-                        "message": "Kategorioiden listaus",
-                        "count": categorylist.count(),
-                        "data": categorylist.serialize()
-                        }, 200
-                else:
-                        return {"message": "Yhtään kategoriaa ei löytynyt"}, 404
+            if new_category.create():
+                return {
+                    "message": "Kategorian luonti onnistui"
+                }, 200
+            else:
+                return {
+                    "message": "Tuntematon virhe"
+                }, 500
+        else:
+            return {
+                "error": "300",
+                "message": "Kategorian luonti epäonnistui.",
+                "invalid_fields": validator.getInvalidFields(),
+                "messages": validator.getInvalidMessages()
+            }, 400
 
+    def get(self):
+        categorymodel = CategoryModel()
+        categorylist = DataModelList(CategoryModel)
 
-
-            
+        if(categorylist.load("SELECT "+categorymodel.getFields()+" FROM "+categorymodel.getTable())):
+            categorylist.computeAll()
+            return {
+                "message": "Kategorioiden listaus",
+                "count": categorylist.count(),
+                "data": categorylist.serialize()
+            }, 200
+        else:
+            return {"message": "Yhtään kategoriaa ei löytynyt"}, 404
