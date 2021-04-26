4c310487faf70dd91172316920eaa1d8f35fd157
Ville Prittinen <villepri@hotmail.com>
2021-04-26 13:43:59 +0300

    OrderedServices lisätty

---
+++ client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/OrderedServices.java
@@ -0,0 +1,14 @@
+package com.example.pienyritysappi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderedServices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ordered_services);
+    }
+}
