a7183a727dbc2dad30f041579db4dce4248df677
Ville Prittinen <villepri@hotmail.com>
2021-04-27 12:56:53 +0300

    OrderedServices activityyn tulee "ei tilauksia"-nappi jos ei saada tilauksia backendiltä

--- client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/OrderedServices.java
+++ client/Pienyritysappi/app/src/main/java/com/example/pienyritysappi/OrderedServices.java
@@ -7,8 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
-import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -20,30 +20,36 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class OrderedServices extends AppCompatActivity {
 
     private String Url = "http://mobiilisovellus.therozor.com:5000/orders";
-    private String orderTitle;
+    private String serviceTitle;
     private String orderPrice;
-    private String orderStatus;
-    private String orderSenderId;
+    private int orderStatus;
+    private String orderStatusString;
+    private String serviceProvider;
     private String buttonText;
+    private String api_key = "A5NG1QCBjxNwikVq2zocyAOtGXw3oZCm";
     private Button nButton;
     private RequestQueue mQueue;
     private int orderCount;
     private JSONArray jsonArray;
     private JSONObject service;
+    private JsonObjectRequest request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordered_services);
         mQueue = Volley.newRequestQueue(this);
-
+        jsonParse();
     }
 
     private void jsonParse() {
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, Url, null,
+        request = new JsonObjectRequest(Request.Method.GET, Url, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
@@ -63,8 +69,25 @@ public class OrderedServices extends AppCompatActivity {
             public void onErrorResponse(VolleyError error) {
                 System.out.println("onErrorResponsessa ollaan");
                 error.printStackTrace();
+                GridLayout layout = (GridLayout)findViewById(R.id.OrderedServicesGridLayout);
+                nButton = new Button(getApplicationContext());
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+                params.setMargins(10,10,10,10);
+                nButton.setLayoutParams(params);
+                nButton.setBackground(nButton.getContext().getDrawable(R.drawable.rounded_button));
+                nButton.setText("ei tilauksia");
+                nButton.setPadding(30,0,30,0);
+                nButton.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+                layout.addView(nButton);
             }
-        });
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("apikey", api_key);
+                return headers;
+            }
+        };
         mQueue.add(request);
     }
 
@@ -74,6 +97,25 @@ public class OrderedServices extends AppCompatActivity {
         try {
             service = jsonArray.getJSONObject(i);
             //tässä kohtaa tarvii saada tolla i-indeksillä sen kyseisen servicen tiedot; palvelunimi, palvelu selostus, mahdollinen kommentti, palvelun tila, yms. ja laittaa ne buttonTextiin
+            serviceTitle = service.getString("service_title");
+            orderPrice = service.getString("service_price");
+            serviceProvider = service.getString("service_provider_name");
+            orderStatus = service.getInt("order_status");
+            switch(orderStatus){
+                case 1:
+                    orderStatusString="Odottaa";
+                    break;
+                case 2:
+                    orderStatusString="Hyväksytty";
+                    break;
+                case 3:
+                    orderStatusString="Tehty";
+                    break;
+                case 4:
+                    orderStatusString="Peruutettu";
+                    break;
+            }
+            buttonText = serviceTitle + "\ntila: " + orderStatusString + "\n" + serviceProvider + "\nhinta: " + orderPrice + " €";
             //mieti myös onClickiä
             GridLayout.LayoutParams params = new GridLayout.LayoutParams();
             params.setMargins(10,10,10,10);
